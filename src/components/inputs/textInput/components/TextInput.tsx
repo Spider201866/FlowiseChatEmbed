ddfe31e2e994aee442cc06f8cfdf9d91d0fbd294
Your Name <you@example.com>
2024-04-02 09:14:17 +0100

    0915

--- src/components/inputs/textInput/components/TextInput.tsx
+++ src/components/inputs/textInput/components/TextInput.tsx
@@ -70,7 +70,7 @@ export const TextInput = (props: Props) => {
         margin: 'auto',
         'background-color': props.backgroundColor ?? defaultBackgroundColor,
         color: props.textColor ?? defaultTextColor,
-        'border-radius': '15px', // Add this line to change the border-radius 1-4-24
+        'border-radius': '18px', // Add this line to change the border-radius 1-4-24
       }}
       onKeyDown={submitWhenEnter}
     >
