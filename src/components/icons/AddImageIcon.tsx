73c6a4d41c6c92c8865791d9af7d8b8f2d5b8100
Your Name <you@example.com>
2024-04-03 20:48:35 +0100

    2048

--- src/components/icons/AddImageIcon.tsx
+++ src/components/icons/AddImageIcon.tsx
@@ -1,5 +1,4 @@
 import { JSX } from 'solid-js/jsx-runtime';
-const defaultButtonColor = '#000000';
 export const AddImageIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -7,7 +6,7 @@ export const AddImageIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
     height="24"
     viewBox="0 0 24 24"
     fill="none"
-    stroke={props.color ?? defaultButtonColor}
+    stroke="#000000"
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
