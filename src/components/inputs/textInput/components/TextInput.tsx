25b3c540585eaa6c67bed4edefd6edf55e41e347
Your Name <you@example.com>
2024-05-19 16:29:09 +0100

    daffy2

--- src/components/inputs/textInput/components/TextInput.tsx
+++ src/components/inputs/textInput/components/TextInput.tsx
@@ -80,7 +80,6 @@ export const TextInput = (props: Props) => {
       }}
       onKeyDown={submitWhenEnter}
     >
-  
       {props.uploadsConfig?.isImageUploadAllowed ? (
         <>
           <ImageUploadButton
