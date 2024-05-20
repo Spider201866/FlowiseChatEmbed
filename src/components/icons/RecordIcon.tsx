5e248fc41a4b734cf9375df8fbc756e21107158b
Your Name <you@example.com>
2024-05-20 18:10:39 +0100

    fred6

--- src/components/icons/RecordIcon.tsx
+++ src/components/icons/RecordIcon.tsx
@@ -11,7 +11,6 @@ export const RecordIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
-    style={{ color: props.color ?? defaultButtonColor }} // Added style attribute
   >
     <path d="M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3Z" />
     <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
