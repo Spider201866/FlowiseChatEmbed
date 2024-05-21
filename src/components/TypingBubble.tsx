1b26da615a37de43410b703828a2981dd1e9d0ae
Your Name <you@example.com>
2024-05-21 13:08:22 +0100

    fred33

--- src/components/TypingBubble.tsx
+++ src/components/TypingBubble.tsx
@@ -1,8 +1,7 @@
-// TypingBubble.tsx
 export const TypingBubble = () => (
-  <div class="bubble-typing" style="background-color: red;"> {/* Debugging background color */}
-    <div class="bubble1" style="background-color: blue;"></div> {/* Debugging background color */}
-    <div class="bubble2" style="background-color: green;"></div> {/* Debugging background color */}
-    <div class="bubble3" style="background-color: yellow;"></div> {/* Debugging background color */}
+  <div class="flex items-center">
+    <div class="w-2 h-2 mr-1 rounded-full bubble1" />
+    <div class="w-2 h-2 mr-1 rounded-full bubble2" />
+    <div class="w-2 h-2 rounded-full bubble3" />
   </div>
 );
